8598d2389d825d382ca045ae6d8b2b6297d22b53
AlexKhotian <doctaww@gmail.com>
2017-03-22 21:44:32 +0100

    File receiver
    File receiver creates own connection to handle related file transfer
    session.

--- FileSession/FileReceiver.go
+++ FileSession/FileReceiver.go
@@ -1,23 +1,50 @@
 package FileSession
 
-import "net"
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
 
 // IFileReceiver receiver file and folders from network.
 type IFileReceiver interface {
 	SetSavePath(filePath *string)
 	ReceiveFileFromConnection(conn *net.Conn)
-	saveFiles()
+
+	initFileReceiver() bool
 }
 
 // fileReceiver implements interface and perfomr save
 type fileReceiver struct {
-	_filePath string
+	_filePath          string
+	_listener          net.Listener
+	_connection        net.Conn
+	_transmissionEnded chan bool
 }
 
 // IFileReceiverFactory creates instance of receiver
 func IFileReceiverFactory() IFileReceiver {
 	fileReceiver := new(fileReceiver)
 	return fileReceiver
+}
+
+func (receiver *fileReceiver) initFileReceiver() bool {
+	listener, err := net.Listen("tcp", "localhost:27111")
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	receiver._listener = listener
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	receiver._connection = conn
+	receiver.ReceiveFileFromConnection(&conn)
+	return true
 }
 
 // SetSavePath set path where to save new saveFiles
@@ -27,17 +54,13 @@ func (receiver *fileReceiver) SetSavePath(filePath *string) {
 
 // ReceiveFileFromConnection starts to receive file
 func (receiver *fileReceiver) ReceiveFileFromConnection(conn *net.Conn) {
-	/*file, err := os.Open(strings.TrimSpace(receiver._filePath))
+	file, err := os.Open(strings.TrimSpace(receiver._filePath))
 	if err != nil {
 		log.Fatal("Failed to create file")
 	}
+	defer file.Close()
 
-	for err != nil && err != io.EOF {
-		bytes, err = *conn.Read(file)
-	}*/
-}
-
-// saveFiles saves files to save path
-func (receiver *fileReceiver) saveFiles() {
-
+	for {
+		io.CopyN(file, *conn, 1024)
+	}
 }
